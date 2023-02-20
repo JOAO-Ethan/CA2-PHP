c229b3ee8287f26a05ce25e3aa36c53320f3286d
JOAO-Ethan <joao.ethan@orange.fr>
2023-02-20 09:42:53 +0000

    Refactor of the code

---
+++ templates/card.php
@@ -0,0 +1,10 @@
+<div class="col">
+    <div class="card bg-light text-info" style="width: 20rem">
+        <a href="mineral.php?id=<?php echo $mineral['id'] ?>">
+            <div class="card-body ">
+                <h2 class="card-title"><?php echo $mineral['name']; ?></h2>
+                <img class="card-img img-responsive h-25" src="<?php echo $mineral['picturePath']; ?>" >
+            </div>
+        </a>
+    </div>
+</div>
---
+++ templates/footer.php
@@ -0,0 +1,8 @@
+<footer class="mt-auto p-4 text-center bg-dark text-light">
+    <div>
+        <p>&copy; <?php echo date("Y"); ?> Ethan JOAO</p>
+    </div>
+</footer>
+<script src="js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
