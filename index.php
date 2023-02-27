9258b03bd03e8f68ca7665e3379ea82515e4a66d
JOAO-Ethan <joao.ethan@orange.fr>
2023-02-27 10:34:00 +0000

    print some random mineral species on the homepage

--- index.php
+++ index.php
@@ -1,11 +1,22 @@
-<?php include "templates/header.php"; ?>
-<main class="container">
-  <div class="starter-template text-center">
-    <h1>Bootstrap starter template</h1>
-    <p class="lead">Use this document as a way to quickly start any new project.<br> All you get is this text and a mostly barebones HTML document.</p>
-  </div>
-    <div>
-        <?php echo "Hello World !"?>
-    </div>
-</main><!-- /.container -->
+<?php include "templates/header.php";
+$query = 'SELECT id, name, picturePath FROM mineral ORDER BY RAND() LIMIT 3';
+$statement = $db->prepare($query);
+$statement->execute();
+$minerals = $statement->fetchAll();
+$statement->closeCursor(); ?>
+    <main class="container">
+        <div class="starter-template text-center">
+            <h1>Mineralogy Database</h1>
+            <p class="lead">Welcome to our website ! We provide some information about mineral species. If you want to
+                browse the ones we currently have, please go to the Mineral List page ! </p>
+        </div>
+        <div class="text-center">
+            <h3>Some of our species</h3>
+            <div class="row align-items-center mb-3 mx-auto">
+                <?php foreach ($minerals as $mineral) :
+                    include "templates/card.php";
+                endforeach; ?>
+            </div>
+        </div>
+    </main><!-- /.container -->
 <?php include "templates/footer.php"; ?>
