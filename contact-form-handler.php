35f738bdc00f4c93b96f3f76e507b08e95a305cc
JOAO-Ethan <joao.ethan@orange.fr>
2023-03-06 10:08:49 +0000

    contact form validation

--- contact-form-handler.php
+++ contact-form-handler.php
@@ -1,7 +1,12 @@
 <?php
 
-$errors = '';
+function validate($data): string
+{
+    return htmlspecialchars(stripslashes($data));
+}
 $myemail = 'd00263493@student.dkit.ie';// <-----Put your DkIT email address here.
+
+$errors = '';
 if(empty($_POST['name'])  ||
    empty($_POST['email']) ||
    empty($_POST['message']) ||
@@ -10,9 +15,11 @@ if(empty($_POST['name'])  ||
     empty($_POST['country'])||
         empty($_POST['city'])||
         empty($_POST['street']) ||
-    emptyy($_POST['pc']))
+    empty($_POST['pc']) ||
+    empty($_POST['county']))
 )
 {
+
     $errors .= "\n Error: all fields are required";
 }
 
@@ -22,26 +29,39 @@ $headers = 'From: '.$myemail."\r\n".
     'X-Mailer: PHP/' . phpversion();
 
 
-$name = $_POST['name'];
-$email_address = $_POST['email'];
-$message = $_POST['message'];
-$phone = $_POST['phone'];
-$request = $_POST['request'];
-$city = $_POST['city'];
-$street = $_POST['street'];
-$postal_code = $_POST['pc'];
+$name = validate($_POST['name']);
+$email_address = validate($_POST['email']);
+$message = validate($_POST['message']);
+$phone = validate($_POST['phone']);
+$request = validate($_POST['request']);
+$city = validate($_POST['city']);
+$street = validate($_POST['street']);
+$postal_code = validate($_POST['pc']);
+$county = validate($_POST["county"]);
+
+if($county === ""){
+    $errors .= "\n Error : Invalid County";
+}
+
+if($request === ""){
+    $errors .= "\n Error : Please select a request";
+}
 
 if (!preg_match(
 "/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/i",
 $email_address))
 {
-    $errors .= "\n Error: Invalid email address";
+    $errors .= "\n Error : Invalid email address";
 }
 
 if(!preg_match(
     "/^0[0-9]{2} [0-9]{3} [0-9]{4}$/i",$phone
 )){
     $errors .="\n Error : Invalid phone number";
+}
+
+if(!preg_match("/^[A-Z][0-9]{2} [A-Z0-9]{4}$/i", $postal_code)){
+    $errors.="\n Error : Invalid postal code";
 }
 
 if( empty($errors))
@@ -53,19 +73,7 @@ if( empty($errors))
 
         mail($to,$email_subject,$email_body,$headers);
         //redirect to the 'thank you' page
-        header('Location: contact-form-thank-you.html');
+        header('Location: contact-form-thank-you.php');
 }
+include "contact.php";
 ?>
-<!DOCTYPE HTML>
-<html>
-<head>
-        <title>Contact form handler</title>
-</head>
-
-<body>
-<!-- This page is displayed only if there is some error -->
-<?php
-echo nl2br($errors);
-?>
-</body>
-</html>
--- contact.php
+++ contact.php
@@ -1,26 +1,65 @@
-<?php include "templates/header.php"; ?>
+<?php include "templates/header.php";
+$counties = ["Carlow",
+    "Cavan",
+    "Clare",
+    "Cork",
+    "Donegal",
+    "Dublin",
+    "Dún Laoghaire–Rathdown",
+    "Fingal",
+    "South Dublin",
+    "Galway",
+    "Kildare",
+    "Kilkenny",
+    "Laois",
+    "Leitrim",
+    "Limerick",
+    "Longford",
+    "Louth",
+    "Mayo",
+    "Meath",
+    "Monaghan",
+    "Offaly",
+    "Roscommon",
+    "Sligo",
+    "Tipperary",
+    "Waterford",
+    "Westmeath",
+    "Wexford",
+    "Wicklow"];
+?>
     <main class="container">
         <h1 class="text-center">Please Contact us</h1>
         <p class="text-center mb-3">Any issue regarding the website ? You want some more information about a mineral ?
+            Join us ?
             Contact us right nom !</p>
-        <form method="POST" name="contactform" action="contact-form/contact-form-handler.php">
+
+        <p class="text-danger">
+            <?php
+            if (!empty($errors)) {
+                echo nl2br($errors);
+            }
+            ?>
+        </p>
+        <form method="POST" name="contactform" action="contact-form-handler.php">
             <div class="row mb-1">
-                <label class="col-sm-2 col-form-label" for='name'>Your Name</label>
-                <div class="col-sm-10"><input class="form-control" type="text" name="name"></div>
+                <label class="col-sm-2 col-form-label" for='name' >Your Name<span class="text-danger">*</span></label>
+                <div class="col-sm-10"><input class="form-control" type="text" name="name" required></div>
             </div>
             <div class="row mb-1">
-                <label class="col-sm-2 col-form-label" for='email'>Email Address</label>
-                <div class="col-sm-10"><input class="form-control" type="text" name="email"></div>
+                <label class="col-sm-2 col-form-label" for='email'>Email Address<span class="text-danger">*</span></label>
+                <div class="col-sm-10"><input class="form-control" type="text" name="email" required></div>
             </div>
             <div class="row mb-1">
-                <label class="col-sm-2 col-form-label" for="phone">Phone number</label>
-                <div class="col-sm-10"><input class="form-control" type="tel" name="phone"></div>
+                <label class="col-sm-2 col-form-label" for="phone">Phone number<span class="text-danger">*</span></label>
+                <div class="col-sm-10"><input class="form-control" type="tel" name="phone"
+                                              pattern="0[0-9]{2} [0-9]{3} [0-9]{4}" required></div>
             </div>
             <div class="row mb-1">
-                <label class="col-sm-2 col-form-label" for="request">Request</label>
+                <label class="col-sm-2 col-form-label" for="request">Request<span class="text-danger">*</span></label>
                 <div class="col-sm-10">
                     <div class="form-check form-check-inline p-0">
-                        <input type="radio" name="request" id="issue" value="issue">
+                        <input type="radio" name="request" id="issue" value="issue" required>
                         <label class="form-check-label" for="issue">
                             Issue
                         </label>
@@ -31,29 +70,43 @@
                             Question
                         </label>
                     </div>
+                    <div class="form-check form-check-inline">
+                        <input type="radio" name="request" id="asso" value="association">
+                        <label class="form-check-label" for="asso">
+                            Join the association
+                        </label>
+                    </div>
                 </div>
             </div>
             <div class="row mb-1">
                 <label class="col-sm-2 col-form-label" for="address">Address</label>
                 <div class="col-sm-10 p-lg-4">
                     <div class="row mb-1">
-                        <label class="col-sm-2" for="pc">Postal Code</label>
-                        <input class="col  form-control" type="text" name="pc">
-                        <label class="col-sm-2 text-center" for="city">City</label>
-                        <input class="col form-control" type="text" name="city">
+                        <label class="col-sm-2" for="county">County<span class="text-danger">*</span></label>
+                        <select class="col form-control" name="county">
+                            <option value="" selected>--Select a County--</option>
+                            <?php
+                            foreach($counties as $county){
+                                echo "<option value=strtolower($county)>$county</option>";
+                            }
+                            ?>
+                        </select>
+                        <label class="col-sm-2 text-center" for="city">City<span class="text-danger">*</span></label>
+                        <input class="col form-control" type="text" name="city" required>
                     </div>
                     <div class="row">
-                            <label class="col-3" for="street">Street name & number</label>
-                            <input class="col form-control" type="text" name="street">
-
+                        <label class="col-3" for="street">Street name & number<span class="text-danger">*</span></label>
+                        <input class="col form-control" type="text" name="street" required>
+                        <label class="col-sm-2" for="pc">Postal Code<span class="text-danger">*</span></label>
+                        <input class="col form-control" type="text" name="pc" pattern="[A-Z][0-9]{2} [A-Z0-9]{4}" required>
                     </div>
                 </div>
             </div>
             <div class="mb-1">
-                <label for='message'>Message :</label> <br>
-                <textarea class="form-control" name="message" rows="5"></textarea>
+                <label for='message'>Message<span class="text-danger">*</span></label> <br>
+                <textarea class="form-control" name="message" rows="10" required></textarea>
             </div>
-            <div class="mx-auto">
+            <div class="mx-auto text-center">
                 <input type="submit" class="btn btn-success" value="Submit">
             </div>
         </form>
