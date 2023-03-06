72773dc9796a515604ffff09b0634a39b6c0f5d8
JOAO-Ethan <joao.ethan@orange.fr>
2023-03-06 10:09:04 +0000

    Footer

--- templates/footer.php
+++ templates/footer.php
@@ -1,6 +1,10 @@
 <footer class="mt-auto p-4 text-center bg-dark text-light">
-    <div>
-        <p>&copy; <?php echo date("Y"); ?> Ethan JOAO</p>
+    <div class="d-flex justify-content-around">
+        <a class="link-light text-decoration-none" href="index.php">Homepage</a>
+        <a class="link-light text-decoration-none" href="mineral_list.php">Mineral List</a>
+        <a class="link-light text-decoration-none" href="contact.php">Contact Us</a>
+        </nav>
+        <div>&copy; <?php echo date("Y"); ?> Ethan JOAO</div>
     </div>
 </footer>
 <script src="js/bootstrap.bundle.min.js"></script>
