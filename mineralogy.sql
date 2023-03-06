bf47a7316df5b4ede1c9e967c4669a0bd5855386
JOAO-Ethan <joao.ethan@orange.fr>
2023-03-06 10:51:41 +0000

    add some minerals in database

--- mineralogy.sql
+++ mineralogy.sql
@@ -41,4 +41,17 @@ VALUES ('Beryl','Be<sub>3</sub>Al<sub>2</sub>Si<sub>6</sub>O<sub>18</sub>',3,7.5
        ('Vanadinite','Pb<sup>2+</sup><sub>5</sub>(V<sup>5+</sup>O<sub>4</sub>)<sub>3</sub>Cl',3,2.5,3,'images/vanadinite.jpg'),
        ('Aragonite','CaCO<sub>3</sub>',2,3.5,4,'images/aragonite.jpg'),
        ('Pyrite','Fe<sup>2+</sup>(S<sub>2</sub>)<sup>2-</sup>',1,6,6.5,'images/pyrite.jpg'),
-       ('Baryte','BaS<sup>6+</sup>O<sub>4</sub>',2,3,3,'images/baryte.jpg');
+       ('Baryte','BaS<sup>6+</sup>O<sub>4</sub>',2,3,3,'images/baryte.jpg'),
+       ('Calcite', 'CaCO<sub>3</sub>', 4, 3,3, 'images/calcite.jpg'),
+       ('Topaz','Al<sub>2</sub>SiO<sub>4</sub>F<sub>2</sub>', 2, 8, 8, 'images/topaz.jpg'),
+       ('Staurolite', 'Fe<sup>2+</sup><sub>2</sub>Al<sub>9</sub>Si<sub>4</sub>O<sub>23</sub>(OH)',6,7,7.5,'images/staurolite.jpg'),
+       ('Eudialyte','Na<sub>15</sub>Ca<sub>6</sub>Fe<sup>2+</sup><sub>3</sub>Zr<sup>4+</sup><sub>3</sub>Si(Si<sub>25</sub>O<sub>73</sub>)(O,OH,H<sub>2</sub>O)<sub>3</sub>(Cl,OH)<sub>2</sub>',4,5,6,'images/eudialyte.jpg'),
+       ('Azurite', 'Cu<sup>2+</sup><sub>3</sub>(CO<sub>3</sub>)<sub>2</sub>(OH)<sub>2</sub>', 6,3.5,4,'images/azurite.jpg'),
+       ('Chalcopyrite','Cu<sup>1+</sup>Fe<sup>3+</sup>S<sup>2-</sup><sub>2</sub>',5,3.5,4,'images/chalcopyrite.jpg'),
+       ('Epidote','Ca<sub>2</sub>Fe<sup>3+</sup>Al<sub>2</sub>(Si<sub>2</sub>O<sub>7</sub>)(SiO<sub>4</sub>)O(OH)',6,6,6,'images/epidote.jpg'),
+       ('Copper', 'Cu', 1, 2.5,3,'images/copper.jpg'),
+       ('Fluorite','CaF<sub>2</sub>',1,4,4,'images/fluorite.jpg'),
+       ('Galena', 'PbS', 1,2.5,2.5,'images/galena.jpg'),
+       ('Forsterite','Mg<sub>2</sub>SiO<sub>4</sub>',2,7,7,'images/forsterite.jpg'),
+       ('Variscite', 'AlPO<sub>4</sub>·2H<sub>2</sub>O',2,3.5,3.5,'images/variscite.jpg'),
+       ('Bismuth', 'Bi', 4,2,2.5,'images/bismuth.jpg');
