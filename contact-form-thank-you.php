bce87ea8c11e52fffa2d02531a7f6b0bd1ed2752
JOAO-Ethan <joao.ethan@orange.fr>
2023-03-10 15:19:21 +0000

    the message is now in the center

--- contact-form-thank-you.php
+++ contact-form-thank-you.php
@@ -1,5 +1,6 @@
 <?php include "templates/header.php"; ?>
+<main class="text-center m-3">
     <h1>Thank you!</h1>
     Thank you for submitting the form. We will contact you soon!
-
+</main>
 <?php include "templates/footer.php"; ?>
