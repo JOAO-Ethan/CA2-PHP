a5587a045a0f604464d40ea3882bca50a72a8f0c
JOAO-Ethan <joao.ethan@orange.fr>
2023-03-10 15:03:11 +0000

    Add a slide show and a banner to the homepage

--- index.php
+++ index.php
@@ -4,18 +4,34 @@ $statement = $db->prepare($query);
 $statement->execute();
 $minerals = $statement->fetchAll();
 $statement->closeCursor(); ?>
+    <div id="head" class="container position-relative">
+        <img id="banner" src="images/banner.jpg">
+        <h1 class="position-absolute top-50 start-50 translate-middle text-white fs-1">Mineralogy Database</h1>
+    </div>
     <main class="container">
         <div class="starter-template text-center">
-            <h1>Mineralogy Database</h1>
             <p class="lead">Welcome to our website ! We provide some information about mineral species. If you want to
-                browse the ones we currently have, please go to the Mineral List page ! </p>
+                browse the ones we currently have, please go to the Mineral List page !
+                Remember that no mineral have "magical" properties that can cure you. The only properties mineral
+                species have are their chemicophysical ones (eg. quartz is piezoelectric, uranium is radioactive, lead
+                is toxic, etc)
+            </p>
         </div>
-        <div class="text-center">
+        <div class="text-center mb-4">
             <h3>Some of our species</h3>
-            <div class="row align-items-center mb-3 mx-auto">
-                <?php foreach ($minerals as $mineral) :
-                    include "templates/card.php";
-                endforeach; ?>
+            <div id="mineralCarousel" class="carousel-slide" data-bs-ride="carousel">
+                <div class="carousel-inner">
+                    <?php foreach ($minerals as $mineral) : ?>
+                        <div class="carousel-item <?php if ($mineral == $minerals[0]) echo 'active'; ?>" data-bs-interval="2000">
+                            <a href="mineral.php?id=<?php echo $mineral['id']; ?>">
+                                <img class="img-fluid" src="<?php echo $mineral['picturePath']; ?>">
+                            </a>
+                            <div class="carousel-caption d-none d-md-block">
+                                <h5 class="text-white-100"><?php echo $mineral['name']; ?></h5>
+                            </div>
+                        </div>
+                    <?php endforeach; ?>
+                </div>
             </div>
         </div>
     </main><!-- /.container -->
